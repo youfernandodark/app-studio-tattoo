424c2c8b9b403531bbb06641cd839d213b801f44
youfernandodark <darkskull.adriask8@hotmail.com>
2026-04-14 11:15:11 -0300

    Delete js/ui.js

--- js/ui.js
+++
@@ -1,4 +0,0 @@
-export function initNavigation() { ... }
-export function openModal(id) { ... }
-export function closeModal(id) { ... }
-// funções de filtro que chamam os respectivos módulos de renderização
