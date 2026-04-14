bc660e1e02731ff2a2627479eac342707fc6a979
youfernandodark <darkskull.adriask8@hotmail.com>
2026-04-14 00:11:44 -0300

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,4 @@
+export function initNavigation() { ... }
+export function openModal(id) { ... }
+export function closeModal(id) { ... }
+// funções de filtro que chamam os respectivos módulos de renderização
