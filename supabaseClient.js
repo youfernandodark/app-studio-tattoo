dd5e3d4a2f161dbd633dc901f840e1412864dfbe
youfernandodark <darkskull.adriask8@hotmail.com>
2026-04-14 00:17:05 -0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-import { SUPABASE_URL, SUPABASE_KEY } from './config.js';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
