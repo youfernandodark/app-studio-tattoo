4dcdf864ffbe3d5bbd3684628af626b51d09d1c2
youfernandodark <darkskull.adriask8@hotmail.com>
2026-04-14 12:14:07 -0300

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'SUA_URL_SUPABASE_AQUI';
+const SUPABASE_URL = 'https://bhymkxsgrghhpqgzqrni.supabase.co';
 const SUPABASE_ANON_KEY = 'SUA_CHAVE_ANONIMA_AQUI';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
