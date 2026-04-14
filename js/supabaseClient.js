f7fd01bcc55d9c1495a61921c8fe1ce9ec79ab32
youfernandodark <darkskull.adriask8@hotmail.com>
2026-04-14 00:16:36 -0300

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,4 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { SUPABASE_URL, SUPABASE_KEY } from './config.js';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
