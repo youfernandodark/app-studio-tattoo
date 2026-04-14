74687d43984a69533be0378baa394b9218abe059
youfernandodark <darkskull.adriask8@hotmail.com>
2026-04-14 00:10:28 -0300

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,5 @@
+export function formatMoney(v) { ... }
+export function formatDate(d) { ... }
+export function formatDateTime(d) { ... }
+export function showAlert(msg, type) { ... }
+export async function testarConexao(supabase) { ... }
